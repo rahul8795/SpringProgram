da8af384a773a84abe93370c864dde9f1904dd66
rahul kumar dubey <36735926+rahul8795@users.noreply.github.com>
2019-11-05 16:32:06 +0530

    Add files via upload

---
+++ spring_app02/src/com/cts/beans/HelloBean.java
@@ -0,0 +1,14 @@
+package com.cts.beans;
+
+public class HelloBean {
+	private String name;
+	public void setName(String name){
+		this.name = name;
+	}
+	public String getName(){
+		return name;
+	}
+	public String sayHello(){
+		return "Hello " + name + " !";
+	}
+}
