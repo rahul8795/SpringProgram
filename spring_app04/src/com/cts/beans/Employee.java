55d8664b85e41f9e2764844afd490ca4bd331f93
rahul kumar dubey <36735926+rahul8795@users.noreply.github.com>
2019-11-07 11:56:07 +0530

    Add files via upload

---
+++ spring_app04/src/com/cts/beans/Employee.java
@@ -0,0 +1,10 @@
+package com.cts.beans;
+
+public class Employee {
+	static{
+		System.out.println("Employee Bean Class Loading....");
+	}
+	public Employee() {
+		System.out.println("Employee Bean Class Object Creaitng");
+	}
+}
