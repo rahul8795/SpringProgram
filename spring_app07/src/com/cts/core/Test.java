c0b34bddc39135339e47423ddcdc610b29657c97
rahul kumar dubey <36735926+rahul8795@users.noreply.github.com>
2019-11-08 09:44:52 +0530

    Add files via upload

--- spring_app07/src/com/cts/core/Test.java
+++ spring_app07/src/com/cts/core/Test.java
@@ -1,10 +1,55 @@
 package com.cts.core;
 
+class ThreadScope extends ThreadLocal<String>{
+	@Override
+	protected String initialValue(){
+		return "No data in this scope";
+	}
+}
+
+class A{
+	void m1() {
+		System.out.println("m1() : Thread1 Scope : "+Thread1.threadScope.get() );
+		System.out.println("m1() : Thread2 Scope : "+Thread2.threadScope.get());
+	}
+	void m2() {
+		System.out.println("m2() : Thread2 Scope : "+Thread2.threadScope.get());
+		System.out.println("m2() : Thread1 Scope : "+Thread1.threadScope.get());
+	}
+}
+
+class Thread1 extends Thread{
+	static ThreadScope threadScope = new ThreadScope();
+	A a;
+	Thread1(A a){
+		this.a = a;
+	}
+	public void run(){
+		threadScope.set("AAA");
+		a.m1();
+	}
+}
+
+class Thread2 extends Thread{
+	static ThreadScope threadScope = new ThreadScope(); 
+	A a;
+	Thread2(A a){
+		this.a = a;
+	}
+	public void run() {
+		threadScope.set("BBB");
+		a.m2();
+	}
+}
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		A a = new A();
+		Thread1 t1 = new Thread1(a);
+		Thread2 t2 = new Thread2(a);
+		
+		t1.start();
+		t2.start();
 	}
 
 }
